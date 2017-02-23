5cd693cbd7da1b2e16e80fc7ca320227fdf2502d
Jack Corrigan <johncorrigan2017@u.northwestern.edu>
2017-02-23 11:50:53 -0600

    Add fitbounds functionality

--- js/chicago_gardens.js
+++ js/chicago_gardens.js
@@ -33,13 +33,11 @@ chicagoGardens = {
   sublayerCommunities  : '',
   whereClause          : '',
   joinClause           : '',
-  wardLayer            : '',
   ward_number          : '',
   neighborhood         : '',
   wardSQL              : '',
   communityareaSQL     : '',
-  addressSQL           : '',
-  communityLayer       : '',
+  gardenSQL            : '',
   cartoFields          : 'the_geom, the_geom_webmercator, growing_site_name, is_growing_site_locked, evidence_of_support_organizations, if_it_s_a_community_garden_is_it_collective_or_allotment, choose_growing_site_types, water, compost_system, structures_and_features, season_extension_techniques, animals, address, food_producing, community_garden, is_growing_site_dormant, latitude, longitude, ownership, other_support_organization, growing_site_website, facebook, is_growing_site_fenced, description, ward, communities, public_contact_info, growing_site_image, municipalities',
 
   // Create geocoder object to access Google Maps API. Add underscore to insure variable safety.
@@ -153,10 +151,6 @@ chicagoGardens = {
   clearSearch: function() {
     if (this.sublayerOne)
       this.sublayerOne.remove();
-    if (this.wardLayer)
-      this.map.removeLayer(this.wardLayer);
-    if (this.communityLayer)
-      this.map.removeLayer(this.communityLayer)
     if (this.centerMark)
       this.map.removeLayer(this.centerMark);
     if (this.radiusCircle)
@@ -232,8 +226,7 @@ chicagoGardens = {
   },
 
   renderMap: function() {
-    var gardenSQL = "select gardens.* from allpublicgardendata as gardens" + chicagoGardens.joinClause + " " + chicagoGardens.whereClause
-    console.log(gardenSQL)
+    chicagoGardens.gardenSQL = "select gardens.* from allpublicgardendata as gardens" + chicagoGardens.joinClause + " " + chicagoGardens.whereClause
       // if (chicagoGardens.filterAddress != "") {
       //   gardenSQL += chicagoGardens.addressSQL;
       // }
@@ -262,7 +255,7 @@ chicagoGardens = {
         },
 
         {
-          sql: gardenSQL,
+          sql: chicagoGardens.gardenSQL,
           cartocss: $('#carto-result-style').html().trim(),
           interactivity: this.cartoFields,
         }
@@ -277,6 +270,7 @@ chicagoGardens = {
       chicagoGardens.sublayerOne.setInteraction(true);
       chicagoGardens.sublayerWards = layer.getSubLayer(0);
       chicagoGardens.sublayerCommunities = layer.getSubLayer(1);
+      console.log(chicagoGardens.sublayerOne)
 
       chicagoGardens.sublayerOne.on('featureOver', function(e, latlng, pos, data, subLayerIndex) {
           $(mapName).css('cursor','pointer');
@@ -300,6 +294,15 @@ chicagoGardens = {
           modalPop(data);
       });
     });
+    
+    var sql = new cartodb.SQL({ user: chicagoGardens.cartoUserName  });
+    sql.getBounds(chicagoGardens.gardenSQL)
+      .done(function(bounds) {
+         chicagoGardens.map.fitBounds(bounds, {maxZoom: 14})
+      });
+
+
+    // chicagoGardens.map.fitBounds(createdLayer.getBounds(), {maxZoom: 14});   
   },
 
 
