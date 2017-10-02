1a02cad64c7d9306bec5dea18538b5cdf43f4228
reginafcompton <reginafcompton@gmail.com>
2017-10-02 10:05:44 -0500

    Hardcode header names for CSV download

--- js/chicago_gardens.js
+++ js/chicago_gardens.js
@@ -425,21 +425,9 @@ var chicagoGardens = {
     sql.execute(chicagoGardens.gardenSQL)
       .done(function(listData) {
         obj_array = listData.rows;
-
-        // Get header names. Filter out unnecessary headers. 
-        header_names = Object.keys(obj_array[0]);
-        headers_to_delete = ["cartodb_id", "the_geom", "the_geom_webmercator", "description", "longitude", "latitude"]
-        indices = []
-
-        $.each(headers_to_delete, function(index, value) {
-          indices.push(header_names.indexOf(value));
-        });
-
-        reverse_indices = indices.reverse();
-
-        $.each(reverse_indices, function(index, value){
-          header_names.splice(value, 1)
-        });
+        
+        // Include only rows with useful information, i.e., suppress info about lat-long, carto_id, etc.
+        header_names = ["growing_site_name", "address", "address2", "growing_site_website", "public_contact_info", "facebook", "ward", "municipalities", "communities", "ownership",  "evidence_of_support_organizations", "is_growing_site_fenced", "is_growing_site_dormant", "food_producing", "compost_system", "is_growing_site_locked", , "other_support_organization", "season_extension_techniques", "structures_and_features", "water", "choose_growing_site_types", "community_garden", "if_it_s_a_community_garden_is_it_collective_or_allotment"]
 
         csv_data = header_names.join(", ") + "\n";
 
